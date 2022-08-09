cb5759ded277f0fc383e40d4c9ddc75536ffdf44
yaswanth2009 <108663991+yaswanth2009@users.noreply.github.com>
2022-08-09 09:59:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,9 +11,9 @@ if __name__ == "__main__" :
         os.makedirs(Config.DOWNLOAD_LOCATION)
     plugins = dict(root="Uploader")
     Tellybots = Tellyhub("@TellyUploaderProBot",
-    bot_token=Config.BOT_TOKEN,
-    api_id=Config.API_ID,
-    api_hash=Config.API_HASH,
+    bot_token=5546691928:AAFRtZZ_3sINot1-4Z2xJD-8wWlj2lqQJlk,
+    api_id=11253846,
+    api_hash=8db4eb50f557faa9a5756e64fb74a51a,
     sleep_threshold=120,
     plugins=plugins)
     Tellybots.run()
